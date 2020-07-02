7eed5987056f7496214159323acf3fc8962f331d
bgvel <bgvelesiani2@gmail.com>
2020-07-02 23:35:20 +0400

    profile/editProfile layouts

---
+++ app/src/main/java/com/c0d3in3/finalproject/ui/profile/EditProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.c0d3in3.finalproject.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c0d3in3.finalproject.R
+
+class EditProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
